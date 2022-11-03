d05fddbd02011500433989cc8468f59ef5877e86
jayukasode23 <jayukasode2398@gmail.com>
2022-11-03 15:53:45 +0530

    Added User  Rule3

---
+++ UserPasswordRule3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistationWithLambdaExpression
+{
+    public class UserPasswordRule3
+    {
+        string UserPass3 = "^[A-Z]{1}[0-9]{1}[A-Z a-z]{7}$";
+
+
+        public string ValidPasswordCheckusingLambda(string password) => Regex.IsMatch(password, UserPass3) ? "Password is Valid" : "Password is Not Valid";
+    }
+}
+
