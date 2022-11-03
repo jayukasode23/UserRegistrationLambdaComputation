2635b2e25f75c642fa5533ddf639c41ae975bcd0
jayukasode23 <jayukasode2398@gmail.com>
2022-11-03 15:49:23 +0530

    Added User PreDefined Number

---
+++ UserMobileNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistationWithLambdaExpression
+{
+    public class UserMobileNumber
+    {
+        string Mobile = "^[91]{2}[\\s][6-9]{1}[0-9]{9}$";
+
+        public string ValidMobilenumberCheckusingLambda(string mobilenumber) => Regex.IsMatch(mobilenumber, Mobile) ? "Mobile Number  is Valid" : "Mobile Number is Not Valid";
+    }
+}
