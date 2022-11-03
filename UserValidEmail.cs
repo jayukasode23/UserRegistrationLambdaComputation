e668bccee9b9485f961f46ddb3d01bd64d99acfa
jayukasode23 <jayukasode2398@gmail.com>
2022-11-03 15:47:50 +0530

    Added User Valid Email

---
+++ UserValidEmail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistationWithLambdaExpression
+{
+    public class UserValidEmail
+    {
+        string Email = "^[a-z]{3,5}['.'][a-z]{3,7}['@'][a-z]{3}$";
+
+        public string ValidEmailCheckusingLambda(string email) => Regex.IsMatch(email, Email) ? "Email is Valid" : "Email is Not Valid";
+
+    }
+}
