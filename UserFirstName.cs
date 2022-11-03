5fd3a7c765390190433103aae80ab1b93bc76eee
jayukasode23 <jayukasode2398@gmail.com>
2022-11-03 15:42:58 +0530

    Added User First Name

---
+++ UserFirstName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistationWithLambdaExpression
+{
+    public class UserFirstName
+    {
+        string Firstname = "^[A-Z]{1}[a-z]{3,8}$";
+
+        public string FirstnameCheckusingLambda(string firstname) => Regex.IsMatch(firstname, Firstname) ? "FirstName is Valid" : "FirstName is Not Valid";
+
+    }
+}
