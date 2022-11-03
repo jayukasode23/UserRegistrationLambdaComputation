3e96901194ac8f64c55e97192c690c888558d7d3
jayukasode23 <jayukasode2398@gmail.com>
2022-11-03 15:52:35 +0530

    Added User Rule2

---
+++ UserPasswordRule2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistationWithLambdaExpression
+{
+    public class UserPasswordRule2
+    {
+        string UserPass2 = "^[A-Z]{1}[A-z a-z]{8,15}$";
+
+        public string ValidPasswordCheckusingLambda(string password) => Regex.IsMatch(password, UserPass2) ? " Password is Valid" : "Password is Not Valid";
+    }
+
+}
