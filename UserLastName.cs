3ebc9d72b2988ff238fa5c466609ad6d24e03dbf
jayukasode23 <jayukasode2398@gmail.com>
2022-11-03 15:44:37 +0530

    Added User Last Name

---
+++ UserLastName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistationWithLambdaExpression
+{
+    public class UserLastName
+    {
+        string Lastname = "^[A-Z]{1}[a-z]{6}$";
+        public string LastnameCheckusingLambda(string lastname) => Regex.IsMatch(lastname, Lastname) ? "LastName is Valid" : "LastName is Not Valid";
+
+    }
+}
